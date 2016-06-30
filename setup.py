d3c9d58dd5b7c21dd2e42938a5c790beaaab664f
Carl Dunham <cdunham@gmail.com>
2016-06-29 22:59:41 -0400

    [FEATURE] Add adapters for saving/loading files

--- setup.py
+++ setup.py
@@ -46,7 +46,13 @@ setup(
     package_data={
         'oneid': ['data/*.yaml'],
     },
-    install_requires=['cryptography>=1.1.2,<2', 'PyYAML>=3.11,<4',
-                      'requests[security]>=2.9.1,<3', 'python-dateutil>=2.4.2,<3',
-                      'pytz>=2015.7', 'six>=1.10.0,<2'],
+    install_requires=[
+        'cryptography>=1.1.2,<2',
+        'PyYAML>=3.11,<4',
+        'requests[security]>=2.9.1,<3',
+        'python-dateutil>=2.4.2,<3',
+        'pytz>=2015.7',
+        'six>=1.10.0,<2',
+        'boto3>=1.3.1,<1.4',
+    ],
 )
